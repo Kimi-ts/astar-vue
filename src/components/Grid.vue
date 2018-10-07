f5f472d8d5101023563781602915f909cf1f3cb6
Kimi-ts <r-g@tut.by>
2018-10-07 14:53:54 +0300

    Add weight calculations

---
+++ src/components/Grid.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="grid">
+        <div class="parent">
+            <div v-for="(value,item) in grid" :key="item.key" class="child">
+                <div class="">
+                    <span class="value" v-bind:class="{start : item == start, finish: item == finish }">
+                        {{item}}
+                    </span>
+                    <span class="weight">{{value.weight}}</span>
+                    <div>
+                        {{value.neibs}}
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div>
+        <div>
+            <button @click="reset">Reset</button>
+        </div>
+        <div>
+            enter start: 
+            <input type="text" v-model="localStart">
+        </div>
+        <div>
+            enter finish: 
+            <input type="text" v-model="localFinish">
+        </div>
+        <div>
+            <button @click ="run">Run</button>
+        </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Grid',
+  data: function() {
+      return {
+                localStart: "2",
+                localFinish: "6"
+      }
+  },
+  computed: {
+      start: function(){
+          return this.$store.state.start;
+      },
+      finish: function(){
+          return this.$store.state.finish;
+      },
+      grid: function(){
+          return this.$store.state.grid
+      }
+  },
+  methods:{
+      run: function(){
+          this.$store.commit("resetWeight");
+          this.$store.commit("setStart", this.localStart);
+          this.$store.commit("setFinish", this.localFinish);
+          this.$store.commit("applyWeights");
+      },
+      reset: function(){
+          this.$store.commit("initGrid");
+      }
+  }
+}
+</script>
+
+<style scoped lang="less">
+    .parent{
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+    }
+    .child{
+
+    }
+    .value{
+        font-size: 200%;
+
+        &.start{
+            color: blue;
+        }
+        &.finish{
+            color: pink;
+        }
+    }
+    .weight{
+     vertical-align: top;   
+    }
+</style>
--- src/store.js
+++ src/store.js
@@ -5,10 +5,87 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    grid: {
+      "1": { value: "1", neibs: ["2", "5"], weight: 0 },
+      "2": { value: "2", neibs: ["1", "3", "6"], weight: 0 },
+      "3": { value: "3", neibs: ["2", "7", "4"], weight: 0 },
+      "4": { value: "4", neibs: ["3", "8"], weight: 0 },
+      "5": { value: "5", neibs: ["1", "6"], weight: 0 },
+      "6": { value: "6", neibs: ["5", "2", "7"], weight: 0 },
+      "7": { value: "7", neibs: ["6", "3", "8"], weight: 0 },
+      "8": { value: "8", neibs: ["4", "7"], weight: 0 }
+    },
+    start: "0",
+    finish: "0"
   },
   mutations: {
+    initGrid() {
+      this.state.grid = {
+        "1": { value: "1", neibs: ["2", "5"], weight: 0 },
+        "2": { value: "2", neibs: ["1", "3", "6"], weight: 0 },
+        "3": { value: "3", neibs: ["2", "7", "4"], weight: 0 },
+        "4": { value: "4", neibs: ["3", "8"], weight: 0 },
+        "5": { value: "5", neibs: ["1", "6"], weight: 0 },
+        "6": { value: "6", neibs: ["5", "2", "7"], weight: 0 },
+        "7": { value: "7", neibs: ["6", "3", "8"], weight: 0 },
+        "8": { value: "8", neibs: ["4", "7"], weight: 0 }
+      };
 
+      this.state.start = 0;
+      this.state.finish = 0;
+    },
+    resetWeight() {
+      for (var key in this.state.grid) {
+        this.state.grid[key].weight = 0;
+      }
+    },
+    applyWeights() {
+      var grid = this.state.grid;
+      var start = this.state.start;
+      var finish = this.state.finish;
+
+      var current = grid[start];
+      if (!current) {
+        return;
+      }
+      current.weight = 1;
+
+      if (start == finish) {
+        return
+      }
+
+      var stack = [];
+      stack.unshift(current);
+
+      while (stack.length > 0) {
+        //get bottom item
+        current = stack[stack.length - 1];
+        if (current.value == finish) {
+          break;
+        }
+
+        for(var i = 0; i < current.neibs.length; i++){
+          var neib = current.neibs[i];
+
+          if (!grid[neib].weight){
+            grid[neib].weight = current.weight + 1;
+            stack.unshift(grid[neib]);
+            if (grid[neib].value == finish){
+              stack = [];
+            }
+          }
+        }
+
+        //remove bottom item
+        stack.pop();
+      }
+    },
+    setStart(state, start) {
+      state.start = start;
+    },
+    setFinish(state, finish) {
+      state.finish = finish;
+    },
   },
   actions: {
 
